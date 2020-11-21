9183c11ec931f48430d79d1906c9b433e595ab1e
Max Kul <kma.memo@gmail.com>
2020-11-21 14:58:42 +0200

    Help message added

--- templates/help_message.go
+++ templates/help_message.go
@@ -1,5 +1,10 @@
 package templates
 
 const HelpMessage = `SlackBot sends message to the Slack channel
+
 echo "[ERROR] Some error details" | /usr/local/bin/slackbot
+
+cat file.txt | slackbot
+
+echo "Text message" | slackbot -config ./config.yml
 `
