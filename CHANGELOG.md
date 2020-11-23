4f4c739dfd92dcbc131c644d32c103e5b8046568
Max Kul <kma.memo@gmail.com>
2020-11-23 10:59:35 +0200

    MacOS release added

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+Slackbot Changelog:
+
+## 1.0.5
+- CHANGELOG added
+
+## 1.0.4
+- Release automation added
---
+++ scripts/changelog.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -x
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+cd ${SCRIPTPATH}
